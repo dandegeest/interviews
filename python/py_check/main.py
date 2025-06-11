f925933cead482a21b8beee9ee043f79c85a14ae
DD <73483425+dandegeest@users.noreply.github.com>
2025-06-11 01:25:02 +0000

    Dan DeGeest getting familiar with Codespaces

--- python/py_check/main.py
+++ python/py_check/main.py
@@ -6,19 +6,23 @@ class FooBar:
     foo: str = "foo"
     bar: str = "bar"
     baz: str = "baz"
-
+    name: str = "Dan"
+    
+    def hello() -> str:
+        return f"My name is {get_name()}"
 
 def get_foo() -> str:
     return FooBar.foo
 
-
 def get_bar() -> str:
     return FooBar.bar
-
 
 def get_baz() -> str:
     return FooBar.baz
 
+def get_name() -> str:
+    return FooBar.name
 
 if __name__ == "__main__":
-    print("\n\nHello World!\n\n")
+    print("\n\nHello World!")
+    print(FooBar.hello())
--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -1,13 +1,17 @@
-from py_check.main import get_bar, get_baz, get_foo
+from py_check.main import get_bar, get_baz, get_foo, get_name, FooBar
 
 
 def test_get_foo_should_return_foo():
     assert get_foo() == "foo"
 
-
 def test_get_bar_should_return_bar():
     assert get_bar() == "bar"
 
-
 def test_get_baz_should_return_baz():
     assert get_baz() == "baz"
+
+def test_get_name_should_return_Dan():
+    assert get_name() == "Dan"
+
+def test_call_hello_should_return_My_name_is_Dan():
+    assert FooBar.hello() == "My name is Dan"
